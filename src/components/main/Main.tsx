6e2a9f8cacd999c3f9f44211c59c928a67242b54
denis-larin-22 <denislarin2017@gmail.com>
2023-12-07 19:22:07 +0200

    Added logout function and button

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -37,11 +37,22 @@ export const Main = () => {
         }
     }
 
+    const logoutHandler = () => {
+        // eslint-disable-next-line no-restricted-globals
+        const confirmation = confirm('Вы уверены, что хотите выйти?');
+        if (confirmation) {
+            localStorage.removeItem('trainig_diary_login');
+            setIsAuthorized(false);
+        }
+        return;
+    }
+
     useEffect(() => {
         if (loginObj !== null) {
             updateWorkoutList(loginObj);
         }
     }, [setList])
+
 
     return (
         <div className="container main">
@@ -56,8 +67,13 @@ export const Main = () => {
                 <>
                     <header className="main__header">
                         <Logo />
-                        <p className="">Your workout diary</p>
-                        <h1 className="">Привет, {loginObj?.name}!</h1>
+                        <p>Your workout diary</p>
+                        <div>
+                            <h1>Привет, {loginObj?.name}!</h1>
+                            <button onClick={logoutHandler}>
+                                <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h280v80H200v560h280v80H200Zm440-160-55-58 102-102H360v-80h327L585-622l55-58 200 200-200 200Z" /></svg>
+                            </button>
+                        </div>
                     </header>
                     <InputForm
                         token={tokenValue}
